6c17db3b420efed630857502664fc99ac9bb42b4
James Coglan <jcoglan@gmail.com>
2012-08-01 16:56:41 +0100

    First attempt at remoteStorage 0.7 based config storage.

---
+++ web/remote_store.js
@@ -0,0 +1,66 @@
+/*
+    remoteStorage client API:
+    
+    getCurrentWebRoot:  function ()
+    getListing:         function (path, cb, context)
+    getMedia:           function (path, cb, context)
+    getObject:          function (path, cb, context)
+    getState:           function (path)
+    on:                 function (eventType, cb, context)
+    remove:             function (path)
+    storeMedia:         function (mimeType, path, data)
+    storeObject:        function (type, path, obj)
+    sync:               function (path, switchVal)
+*/
+
+RemoteStore = (function() {
+  if (!window.localStorage || !window.remoteStorage) return null;
+  
+  var ITEM_TYPE = 'password-gen-params';
+  
+  remoteStorage.defineModule('vault', function(client) {
+    // Dummy saved data
+    client.storeObject('password-gen-params', 'global', {length: 16});
+    client.storeObject('password-gen-params', 'services.google', {length: 20, symbol: 0, space: 0});
+    client.storeObject('password-gen-params', 'services.twitter', {length: 10, repeat: 2, upper: 3});
+    
+    var store = {
+      clear: function(callback, context) {
+        var keys = client.getListing('');
+        for (var i = 0, n = keys.length; i < n; i++) client.remove(keys[i]);
+        callback.call(context, null);
+      },
+      
+      load: function(callback, context) {
+        var keys   = client.getListing(''),
+            config = {global: {}, services: {}},
+            key, object;
+        
+        for (var i = 0, n = keys.length; i < n; i++) {
+          key    = keys[i];
+          object = client.getObject(key);
+          delete object['@type'];
+          
+          if (key === 'global') config.global = object;
+          if (/^services\./.test(key)) config.services[key.replace(/^services\./, '')] = object;
+        }
+        callback.call(context, null, config);
+      },
+      
+      dump: function(config, callback, context) {
+        if (config.global) client.storeObject(ITEM_TYPE, 'global', config.global);
+        if (config.services) {
+          for (var key in config.services)
+            client.storeObject(ITEM_TYPE, 'services.' + key, config.services[key]);
+        }
+        callback.call(context, null);
+      }
+    };
+    
+    return {name: 'vault', exports: store};
+  });
+  
+  remoteStorage.loadModule('vault');
+  return remoteStorage.vault;
+})();
+
